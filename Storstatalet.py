6224d707e5cd63ab56f2a63270cf9c11ecb74f05
aiex800 <bertbadar@gmail.com>
2020-05-19 14:59:55 +0200

    kLAR

--- Storstatalet.py
+++ Storstatalet.py
@@ -1,12 +1,12 @@
-störstaTal = 0
-lista = []
-print("Hur mångra siffror är det i listan?")
-antal = int(input(""))
-for x in range(antal):
-    tal = int(input("Skriv ett tal: "))
-    lista.append(tal)
+störstaTal = 0 
+lista = [] #gör en lista där alla siffror sparas
+print("Hur mångra siffror är det i listan?") 
+antal = int(input("")) #Skriver in hur mångra tal man ska ha i listan
+for x in range(antal): #Kör kod stycket x antal gånger
+    tal = int(input("Skriv ett tal: ")) #Skriver in talen
+    lista.append(tal) #Lägger till talet man skriver in i listan
 print(lista)
-for g in lista:
-    if g > störstaTal:
-        störstaTal = g
+for g in lista: #Kör igenom alla tal
+    if g > störstaTal: #om g är större en störstartal
+        störstaTal = g 
 print("största talet är: ", störstaTal)
